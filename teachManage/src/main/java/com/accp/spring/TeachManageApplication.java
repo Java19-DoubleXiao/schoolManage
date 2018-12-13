91f8f55d6328b823dde3be37224856b351dda643
JC7H <2584126420@qq.com>
2018-12-13 14:54:35 +0800

    222

---
+++ teachManage/src/main/java/com/accp/spring/TeachManageApplication.java
@@ -0,0 +1,14 @@
+package com.accp.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeachManageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeachManageApplication.class, args);
+	}
+
+}
+
