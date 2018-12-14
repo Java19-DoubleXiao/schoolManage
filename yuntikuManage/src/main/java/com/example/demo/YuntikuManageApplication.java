bcd4d1c88fc0bfc3720d77057c488e0ed562fdd2
cmylj <1073374453@qq.com>
2018-12-14 16:24:09 +0800

    第一次提交项目

---
+++ yuntikuManage/src/main/java/com/example/demo/YuntikuManageApplication.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YuntikuManageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YuntikuManageApplication.class, args);
+	}
+
+}
+
