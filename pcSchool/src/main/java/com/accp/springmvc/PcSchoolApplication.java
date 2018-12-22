2d054107b43885551f71fde956a3308cff8e7206
lovetocarrot <2394650959@qq.com>
2018-12-22 09:28:38 +0800

    pc端提交配置

--- pcSchool/src/main/java/com/accp/springmvc/PcSchoolApplication.java
+++ pcSchool/src/main/java/com/accp/springmvc/PcSchoolApplication.java
@@ -1,9 +1,13 @@
 package com.accp.springmvc;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@MapperScan(basePackages= {"com.accp.springmvc"})/*扫描mapper*/
+@EnableCaching
 public class PcSchoolApplication {
 
 	public static void main(String[] args) {
