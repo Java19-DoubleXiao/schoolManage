a285e03df06fb583456efec8c6c69d4edd050dcd
JC7H <2584126420@qq.com>
2019-01-13 11:29:18 +0800

    提交登录部分（nana的）

--- teachManage/src/main/java/com/accp/spring/controller/TeacherController.java
+++ teachManage/src/main/java/com/accp/spring/controller/TeacherController.java
@@ -1,5 +1,39 @@
 package com.accp.spring.controller;
 
-public class TeacherController {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import com.accp.spring.pojo.Teacher;
+import com.accp.spring.service.TeacherService;
+@CrossOrigin
+@RestController
+@RequestMapping("/tea")
+public class TeacherController {
+	@Autowired
+	TeacherService teacherService;
+	
+	@PostMapping("/UpdateImage")
+	public int UpdateImage(String teaImage,int teaId ) {
+		String pImage="data:image/png;base64,"+teaImage;
+		return this.teacherService.UpdateImage(pImage, teaId);
+	}
+	
+	@PostMapping("/login")
+	public Teacher login(String teaUserName,String teaPwd,boolean emember) {
+		return this.teacherService.login(teaUserName, teaPwd,emember);
+	}
+	
+	
+	
+	
+	@GetMapping("/loginIn")
+	public Object loginIn(String teaUserName) {
+		return this.teacherService.loginIn(teaUserName);
+	}
+	
 }
